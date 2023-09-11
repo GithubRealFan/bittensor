146e04dfe4af4235e2b7f6778e0a9e1aebe0da69
GithubRealFan <christianlee92227@gmail.com>
2023-09-11 04:35:30 -0400

    added one

--- neurons/text/prompting/miners/huggingface/robertmyers/server.py
+++ neurons/text/prompting/miners/huggingface/robertmyers/server.py
@@ -16,7 +16,7 @@ class RobertMyersProcessor:
         self.model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
         self.pipeline = pipeline(
             "text-generation", self.model, tokenizer=self.tokenizer,
-            device=device, max_new_tokens=255, temperature=0.11, do_sample=True, pad_token_id=self.tokenizer.eos_token_id
+            device=device, max_new_tokens=255, temperature=0.09, do_sample=True, pad_token_id=self.tokenizer.eos_token_id
         )
 
     def promptToMessages(self, prompt):
